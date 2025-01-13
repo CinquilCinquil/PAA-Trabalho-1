07b2052e0aa7f4c97dcfc3fcd214894554df03dd
Ian Gabriel <ianeigor12@gmail.com>
2025-01-13 16:20:29 -0300

    add script to process results

---
+++ process_test_results.py
@@ -0,0 +1,63 @@
+import os
+import sys
+N_VARIABLES=0;
+N_CLAUSES=1;
+
+V_DISTANCE=0;
+V_GUESS=2;
+
+
+def process_test_results(algorithm, repetitions):
+  instances = [[20, 91], [50, 218], [125, 538]]
+  output_file_path = os.path.join("test_results", f"processed_results_{algorithm}_{repetitions}rep.txt")
+  output_file = open(output_file_path, 'w');
+
+  output_file.write("variables,clauses,average time,average % worse than the optimal,correct %\n")
+  for instance in instances:
+    composed_results_path = os.path.join(
+      "test_results", 
+      f"composed_results_{algorithm}_{repetitions}rep_test_cases_uf{instance[N_VARIABLES]}-{instance[N_CLAUSES]}.txt"
+    )
+
+    composed_results_file = open(composed_results_path, 'r')
+    lines = composed_results_file.readlines();
+
+    best_solution = dict();
+
+    total_time = 0
+    total_correct = 0
+    total_percent = 0
+
+    for index,line in enumerate(lines):
+      if(index == 0): continue;
+
+      name, guess, distance, solution, time, seed = line.split(", ")
+      # print(distance)
+      if not best_solution.__contains__(name):
+        best_solution[name] = (int(distance), float(time), int(guess));
+      elif best_solution[name][V_DISTANCE] < int(distance):
+        best_solution[name] = (int(distance), float(time), int(guess));
+      total_time += float(time)
+
+    for name in best_solution:
+      solution = best_solution[name]
+      total_correct += solution[V_GUESS];
+      total_percent += float((instance[N_CLAUSES]-solution[V_DISTANCE])/instance[N_CLAUSES]);
+
+    average_time = total_time/(len(best_solution)*repetitions)
+    average_percentage = total_percent/len(best_solution)
+    average_correctness = total_correct/len(best_solution)
+
+    output_file.write(f"{instance[N_VARIABLES]}, {instance[N_CLAUSES]}, {average_time}, {average_percentage}, {average_correctness}\n")
+  
+  output_file.close()
+
+
+"""
+usage: [ALGORITHM] [REPETITIONS]
+"""
+if __name__ == '__main__':
+    if len(sys.argv) < 3: 
+      print("Not enough arguments")
+    else: 
+      process_test_results(sys.argv[1], sys.argv[2])
--- user_interface.py
+++ user_interface.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 from run_tests import run_tests
 from compose_test_results import compose_test_results
+from process_test_results import process_test_results
 
 subprocess.run(["g++", "main.cpp", "-o", "main.exe"], shell=False)
 
@@ -10,20 +11,22 @@ print("\n### WELCOME ###\n")
 print("This is a menu where you can execute tests with the available SAT solvers.\n")
 
 current_alg = "CIDPLL"
+repetitions = 5
 
 while True:
     print("Type:\n")
     print("   1 to run all tests files on a folder;\n")
     print("   2 to run a single test file;\n")
     print(f"   3 to switch the algorithm. The current one is {current_alg};\n")
-    print("   4 exit.\n")
+    print("   4 to run all tests available;\n")
+    print("   5 exit.\n")
     selected_option = int(input("Input: "))
 
     if selected_option == 1:
         filepath = input("Please enter the path to the folder containing tests: ")
-        print("running with 5 repetitions");
-        run_tests(current_alg, filepath, 5)
-        compose_test_results(current_alg, filepath, 5)
+        print(f"running with {repetitions} repetitions");
+        run_tests(current_alg, filepath, repetitions)
+        compose_test_results(current_alg, filepath, repetitions)
         print("All finished")
     elif selected_option == 2:
         filepath = input("Please enter the path to the test file: ")
@@ -40,7 +43,14 @@ while True:
         algorithms = ["CIDPLL", "DPLL", "GENETIC", "TS"]
         current_alg = algorithms[select_alg-1]
     elif selected_option == 4:
-        break
+        folders = ["uf20-91", "uf50-218", "uf125-538"]
+        for fold in folders:
+            run_tests(current_alg, os.path.join("test_cases", fold), repetitions)
+            compose_test_results(current_alg, os.path.join("test_cases", fold), repetitions)
+        process_test_results(current_alg, repetitions)
+
+    elif selected_option == 5:
+        break;
     else:
         print("Invalid input!")
         
